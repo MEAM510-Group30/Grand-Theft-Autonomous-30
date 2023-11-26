44339e20c3b3ef0c653ebcebd547854ae784547e
jbwenjoy <jbwenjoy@outlook.com>
2023-11-26 15:05:27 -0500

    Update

--- src/esp32s2/actions.h
+++ src/esp32s2/actions.h
@@ -5,6 +5,15 @@
 
     Two layers of actions should be defined here:
         1. Basic actions, like specifying motor speed, servo angle, etc.
-        2. Upper-layer actions, like moving, turning, 
+        2. Upper-layer actions, like moving, turning, etc, which are combinations of basic actions.
 
 */
+
+#ifndef ACTIONS_H
+#define ACTIONS_H
+
+
+
+
+
+#endif
--- src/esp32s2/communication.h
+++ src/esp32s2/communication.h
@@ -1,1 +1,7 @@
 // I'm still thinking about whether this function should be made a seperete header file or just within the ino file.
+
+#ifndef COMMUNICATION_H
+#define COMMUNICATION_H
+
+
+#endif
