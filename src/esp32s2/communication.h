4fb7dd7e75fc4e6dfe091578699e5b6c4b481a27
YuruiWang821 <yuruiw@seas.upenn.edu>
2023-11-30 21:33:39 -0500

    update

--- src/esp32s2/communication.h
+++ src/esp32s2/communication.h
@@ -5,10 +5,11 @@
 
 #include "website.h"
 #include "html510.h"
+#include "s2_vive510.h"
 class web_commun
 {
     Actions action;
-    int speed = 0;
+    Behavior behavior;
 
     web_commun(IPAddress local_IP, const char *ssid = "Furina", const char *pwd = "Furinaaa") :
     {
@@ -26,28 +27,91 @@ class web_commun
 
         html_server.begin();
         html_server.attachHandler("/", handleRoot);
-        html_server.attachHandler("/autopilot_on", handleAutopilotOn);
-        html_server.attachHandler("/autopilot_off", handleAutopilotOff);
+        // html_server.attachHandler("/autopilot_on", handleAutopilotOn);
+        // html_server.attachHandler("/autopilot_off", handleAutopilotOff);
+        html_server.attachHandler("/tro", behavior.trophyMoving());
+        html_server.attachHandler("/man", behavior.fullyManual());
+        html_server.attachHandler("/wall", behavior.wallFollowing());
+        html_server.attachHandler("/car", carPushing());
+        html_server.attachHandler("/F", action.moveForward(action.speed));
+        html_server.attachHandler("/B", action.moveBackward(action.speed));
+        html_server.attachHandler("/L", action.turnLeft(action.speed, action.turnRate));
+        html_server.attachHandler("/R", action.turnRight(action.speed, action.turnRate));
+        html_server.attachHandler("/O", action.stop());
         html_server.attachHandler("/speed_slider=", handleSpeed);
         html_server.attachHandler("/turn_rate_slider=", handleTurnRate);
-        html_server.attachHandler("/F", action.moveForward(speed));
-        html_server.attachHandler("/B", action.moveBackward(speed));
-        html_server.attachHandler("/L", action.turnLeft(speed, turnRate));
-        html_server.attachHandler("/R", action.turnRight(speed, turnRate));
-        html_server.attachHandler("/O", action.stop());
-        html_server.attachHandler("/S", action.stop());
-        html_server.attachHandler("/+", handleSpeedUp);
-        html_server.attachHandler("/-", handleSlowDown);
     }
 
     ~web_commun() {}
 
-    void handleSpeedUp(){
-        action.speed += 500;
+    void handleRoot()
+    {
+        html_server.sendhtml(website);
     }
 
-    void handleSlowDown(){
-        action.speed -= 500;
+    void handleSpeed()
+    {
+        action.speed = html_server.getVal();
+    }
+
+    void handleTurnRate()
+    {
+        action.turnRate = html_server.getVal();
+    }
+}
+
+class UDP_broadcast
+{
+    int signalPin 4; // GPIO pin receiving signal from Vive circuit
+    Vive510 vive1(signalPin);
+
+    WiFiUDP UDPServer;
+    const char *ssid = "TP-Link_FD24";
+    const char *password = "65512111";
+    IPAddress target(192, 168, 1, 58);  // change to IP you are sending to
+    IPAddress myIP(192, 168, 1, 57);    //change our IP
+    char udpBuffer[100];
+
+    void fncUdpSend()
+    {
+        // send what ever you want upto buffer size
+        UDPServer.beginPacket(target, 2808); // send to UDPport 2808
+        UDPServer.printf("%s", this.udpBuffer);
+        UDPServer.endPacket();
+        Serial.println(udpBuffer);
+    }
+
+    UDP_broadcast()
+    {
+        Serial.begin(115200);
+
+        WiFi.begin(ssid, password);
+
+        WiFi.config(myIP,                         // device IP address
+                    IPAddress(192, 168, 1, 1),    // gateway (not used)
+                    IPAddress(255, 255, 255, 0)); // netmask
+
+        UDPServer.begin(2808); // 2808 arbitrary UDP port#
+        while (WiFi.status() != WL_CONNECTED)
+        {
+            delay(500);
+            Serial.print(".");
+        }
+        Serial.printf("WiFi connected to %s", ssid);
+        Serial.print("Sending messages from ");
+        Serial.print(myIP);
+        Serial.print(" to ");
+        Serial.println(target);
+    }
+    
+    void sendXY()
+    {
+        if (vive1.status() == VIVE_LOCKEDON)
+        {
+            Serial.printf("X %d, Y %d\n", vive1.xCoord(), vive1.yCoord());
+        }
+        else
+            vive1.sync(15); // try to resync 15 times (nonblocking);
     }
 }
 
