0c38d813d952aad6168906e250adf952e5baedb8
YuruiWang821 <yuruiw@seas.upenn.edu>
2023-12-08 19:15:53 -0500

    update c3_I2C.h

--- src/esp32c3/c3_I2C.h
+++ src/esp32c3/c3_I2C.h
@@ -6,65 +6,35 @@
 #define C3_I2C_H
 
 // Include any necessary headers here
-class I2C_slave_commun
-{
-#include <stdio.h>
-#include "esp_log.h"
-#include "driver/i2c.h"
-#include "sdkconfig.h"
+class Serial_commun
+{ // use this class in main to communicate between two boards. to initialize, give tx and rx
+public:
+    String message;
+    SoftwareSerial softSerial;
 
-#define DATA_LENGTH 128   /*!< Data buffer length of test buffer */
-#define RW_TEST_LENGTH 22 /*!< Data length for r/w test, [0,DATA_LENGTH] */
-
-#define I2C_SLAVE_SCL_IO (gpio_num_t)4         /*!< gpio number for i2c slave clock */
-#define I2C_SLAVE_SDA_IO (gpio_num_t)5         /*!< gpio number for i2c slave data */
-#define I2C_SLAVE_TX_BUF_LEN (2 * DATA_LENGTH) /*!< I2C slave tx buffer size */
-#define I2C_SLAVE_RX_BUF_LEN (2 * DATA_LENGTH) /*!< I2C slave rx buffer size */
-
-#define ESP_SLAVE_ADDR 0x28 /*!< ESP32 slave address, you can set any 7bit value */
-uint8_t data_rd[DATA_LENGTH];   //data_rd store the data read
-
-    I2C_slave_commu()
+    Serial_commun(int RX_PIN, int TX_PIN)
+        : softSerial(RX_PIN, TX_PIN)
     {
-        Serial.begin(115200); // put your setup code here, to run once:
-        i2c_slave_init();
+        Serial.begin(115200);
+        softSerial.begin(9600);
     }
 
-    ~I2C_slave_commun() {}
+    ~Serial_commun() {}
 
-    static esp_err_t i2c_slave_init()
+    void read()
     {
-        i2c_port_t i2c_slave_port = I2C_NUM_0;
-        i2c_config_t conf_slave;
-        conf_slave.sda_io_num = I2C_SLAVE_SDA_IO;
-        conf_slave.sda_pullup_en = GPIO_PULLUP_ENABLE;
-        conf_slave.scl_io_num = I2C_SLAVE_SCL_IO;
-        conf_slave.scl_pullup_en = GPIO_PULLUP_ENABLE;
-        conf_slave.mode = I2C_MODE_SLAVE;
-        conf_slave.slave.addr_10bit_en = 0;
-        conf_slave.slave.slave_addr = ESP_SLAVE_ADDR;
-        i2c_param_config(i2c_slave_port, &conf_slave);
-        return i2c_driver_install(i2c_slave_port, conf_slave.mode,
-                                  I2C_SLAVE_RX_BUF_LEN,
-                                  I2C_SLAVE_TX_BUF_LEN, 0);
-    }
-
-    void I2C_slave_read()
-    {
-        if (i2c_slave_read_buffer(I2C_NUM_0, data_rd, RW_TEST_LENGTH, 0) > 0)
-        { // last term is timeout period, 0 means don't wait
-            Serial.printf("READ from master: %s\n", data_rd);
-            //                         I2Cport    buffer   length of data   max ticks to wait if buffer is full
-        }
-    }
-
-    void I2C_slave_write(uint8_t *data_wr)
-    {
-        if (i2c_slave_write_buffer(I2C_NUM_0, data_wr, RW_TEST_LENGTH, 10 / portTICK_RATE_MS))
+        if (softSerial.available())
         {
-            Serial.printf("WRITE to master: %s\n", data_wr);
+            message = softSerial.readString();
+            Serial.println("Received message on ESP32 2: " + message);
         }
     }
-}
+
+    void write(String wr_message)
+    {
+        Serial.println("Sending message: " + wr_message);
+        softSerial.println(wr_message);
+    }
+};
 
 #endif
--- src/esp32s2/communication.h
+++ src/esp32s2/communication.h
@@ -31,182 +31,213 @@ enum commun_Actions
     commun_STOP
 };
 
-class esp_now {    // remember to initialize; use member function sendMessage() to send group number
-#define CHANNEL 1  // channel can be 1 to 14, channel 0 means current channel.
+class esp_now
+{                 // remember to initialize; use member function sendMessage() to send group number
+#define CHANNEL 1 // channel can be 1 to 14, channel 0 means current channel.
 
 public:
-  int groupNumber = 30;
-  String message = String(groupNumber);
-  esp_now_peer_info_t peer1;
-  uint8_t MAC_RECV[6] = { 0x68, 0x67, 0x25, 0x82, 0x8C, 0xCC };  // receiver MAC address
+    int groupNumber = 30;
+    String message = String(groupNumber);
+    esp_now_peer_info_t peer1;
+    uint8_t MAC_RECV[6] = {0x68, 0x67, 0x25, 0x82, 0x8C, 0xCC}; // receiver MAC address
 
-  esp_now() {
-    memcpy(peer1.peer_addr, MAC_RECV, 6);
-    peer1.channel = 0;  // Set your channel here
-    peer1.encrypt = false;
-  }
-
-  ~esp_now() {}
-
-  void initialize(){
-    Serial.begin(115200);
-    WiFi.mode(WIFI_STA);
-    Serial.print("Sending MAC: ");
-    Serial.println(WiFi.macAddress());
-    if (esp_now_init() != ESP_OK) {
-      Serial.println("init failed");
-      ESP.restart();
-    } else Serial.println("init succeed");
-    // esp_now_register_send_cb(OnDataSent); // optional if you want ack interrupt
-
-    if (esp_now_add_peer(&peer1) != ESP_OK) {
-      Serial.println("Pair failed");  // ERROR  should not happen
-    } else Serial.println("Pair succeed");
-
-    esp_now_register_send_cb(OnDataSent);
-  }
-
-  static void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-    if (status == ESP_NOW_SEND_SUCCESS)
-      Serial.println("Success ");
-    else
-      Serial.println("Fail ");
-  }
-
-  void sendMessage() {  // use in main function to send group number every time communicate with web       // message is the group number
-    if (esp_now_send(MAC_RECV, (uint8_t *)message.c_str(), message.length()) == ESP_OK)
-      Serial.printf("Sent '%s' to %x:%x:%x:%x:%x:%x \n", message, peer1.peer_addr[0], peer1.peer_addr[1], peer1.peer_addr[2], peer1.peer_addr[3], peer1.peer_addr[4], peer1.peer_addr[5]);
-    else {
-      esp_err_t sendResult = esp_now_send(peer1.peer_addr, reinterpret_cast<const uint8_t *>(message.c_str()), message.length());
-      Serial.printf("Send failed, error code: %d\n", sendResult);
+    esp_now()
+    {
+        memcpy(peer1.peer_addr, MAC_RECV, 6);
+        peer1.channel = 0; // Set your channel here
+        peer1.encrypt = false;
     }
-  }
+
+    ~esp_now() {}
+
+    void initialize()
+    {
+        if (esp_now_init() != ESP_OK)
+        {
+            Serial.println("init failed");
+            ESP.restart();
+        }
+        else
+            Serial.println("init succeed");
+        // esp_now_register_send_cb(OnDataSent); // optional if you want ack interrupt
+
+        if (esp_now_add_peer(&peer1) != ESP_OK)
+        {
+            Serial.println("Pair failed"); // ERROR  should not happen
+        }
+        else
+            Serial.println("Pair succeed");
+
+        esp_now_register_send_cb(OnDataSent);
+    }
+
+    void WiFi_initial()
+    {
+        Serial.begin(115200);
+        WiFi.mode(WIFI_STA);
+        Serial.print("Sending MAC: ");
+        Serial.println(WiFi.macAddress());
+    }
+
+    static void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+    {
+        if (status == ESP_NOW_SEND_SUCCESS)
+            Serial.println("Success ");
+        else
+            Serial.println("Fail ");
+    }
+
+    void sendMessage()
+    { // use in main function to send group number every time communicate with web       // message is the group number
+        if (esp_now_send(MAC_RECV, (uint8_t *)message.c_str(), message.length()) == ESP_OK)
+            Serial.printf("Sent '%s' to %x:%x:%x:%x:%x:%x \n", message, peer1.peer_addr[0], peer1.peer_addr[1], peer1.peer_addr[2], peer1.peer_addr[3], peer1.peer_addr[4], peer1.peer_addr[5]);
+        else
+        {
+            esp_err_t sendResult = esp_now_send(peer1.peer_addr, reinterpret_cast<const uint8_t *>(message.c_str()), message.length());
+            Serial.printf("Send failed, error code: %d\n", sendResult);
+        }
+    }
 };
 
-class web_commun {  // in main function, initialize a web_commun variable to attach handlers
-  // visit commun_Mode, Action, speed, turnRate in main function
-#define portMAX_DELAY 4294967295UL
+class web_commun
+{   // in main function, initialize a web_commun variable to attach handlers
+    // visit commun_Mode, Action, speed, turnRate in main function
+
 public:
-  static esp_now esp_now_message;
-  static commun_Mode mode;
-  static commun_Actions action;
-  static int speed;
-  static int turnRate;
-  static WiFiServer wifi_server;
-  static HTML510Server html_server;
-  const char *ssid = "group30";  //set my router ID
-  const char *pwd = "12345678";  //set router password
-  static SemaphoreHandle_t espNowSemaphore;
+    static esp_now esp_now_message;
+    static commun_Mode mode;
+    static commun_Actions action;
+    static int speed;
+    static int turnRate;
+    static WiFiServer wifi_server;
+    static HTML510Server html_server;
+    const char *ssid = "group30"; // set my router ID
+    const char *pwd = "12345678"; // set router password
+    static SemaphoreHandle_t espNowSemaphore;
 
-  web_commun() {
-    espNowSemaphore = xSemaphoreCreateMutex();
-  }
+    web_commun()
+    {
+        espNowSemaphore = xSemaphoreCreateMutex();
+    }
 
-  void initial(IPAddress local_IP, const char *ssid = "group30", const char *pwd = "12345678") {
-    // our own IP address is local_IP above
-    IPAddress gateway_IP(192, 168, 1, 1);
-    IPAddress subnet_IP(255, 255, 255, 0);
+    void initial(IPAddress local_IP, const char *ssid = "group30", const char *pwd = "12345678")
+    {
+        // our own IP address is local_IP above
+        IPAddress gateway_IP(192, 168, 1, 1);
+        IPAddress subnet_IP(255, 255, 255, 0);
 
-    Serial.begin(115200);
-    WiFi.mode(WIFI_MODE_AP);  // wifi in ap mode, no router
-    Serial.print("Access point ");
-    Serial.println(ssid);
-    WiFi.softAP(ssid, pwd);
-    WiFi.softAPConfig(local_IP, gateway_IP, subnet_IP);
-    Serial.print("AP IP address ");
-    Serial.println(local_IP);
-    //wifi_server.begin();
-    //IPAddress softAP_IP = WiFi.softAPIP();
-    esp_now_message.initialize();
+        Serial.begin(115200);
+        WiFi.mode(WIFI_MODE_AP); // wifi in ap mode, no router
+        Serial.print("Access point ");
+        Serial.println(ssid);
+        WiFi.softAP(ssid, pwd);
+        WiFi.softAPConfig(local_IP, gateway_IP, subnet_IP);
+        Serial.print("AP IP address ");
+        Serial.println(local_IP);
+        // wifi_server.begin();
+        // IPAddress softAP_IP = WiFi.softAPIP();
+        esp_now_message.initialize();
 
-    html_server.begin();
-    html_server.attachHandler("/", handleRoot);
-    html_server.attachHandler("/tro", handleTrophy);
-    html_server.attachHandler("/man", handleManual);
-    html_server.attachHandler("/wall", handleWall);
-    html_server.attachHandler("/car", handleCar);
-    html_server.attachHandler("/F", handleForward);
-    html_server.attachHandler("/B", handleBackward);
-    html_server.attachHandler("/L", handleLeft);
-    html_server.attachHandler("/R", handleRight);
-    html_server.attachHandler("/O", handleStop);
-    html_server.attachHandler("/speed_slider=", handleSpeed);
-    html_server.attachHandler("/turn_rate_slider=", handleTurnRate);
+        html_server.begin();
+        html_server.attachHandler("/", handleRoot);
+        html_server.attachHandler("/tro", handleTrophy);
+        html_server.attachHandler("/man", handleManual);
+        html_server.attachHandler("/wall", handleWall);
+        html_server.attachHandler("/car", handleCar);
+        html_server.attachHandler("/F", handleForward);
+        html_server.attachHandler("/B", handleBackward);
+        html_server.attachHandler("/L", handleLeft);
+        html_server.attachHandler("/R", handleRight);
+        html_server.attachHandler("/O", handleStop);
+        html_server.attachHandler("/speed_slider=", handleSpeed);
+        html_server.attachHandler("/turn_rate_slider=", handleTurnRate);
 
-    espNowSemaphore = xSemaphoreCreateMutex();
-  }
+        espNowSemaphore = xSemaphoreCreateMutex();
+    }
 
-  ~web_commun() {}
+    ~web_commun() {}
 
-  static void handleRoot() {
-    html_server.sendhtml(body);
-  }
+    static void handleRoot()
+    {
+        html_server.sendhtml(body);
+    }
 
-  static void handleTrophy() {
-    //esp_now_message.sendMessage();
-    mode = commun_TROPHY;
-    Serial.print("Trophy");
-  }
+    static void handleTrophy()
+    {
+        // esp_now_message.sendMessage();
+        mode = commun_TROPHY;
+        Serial.print("Trophy");
+    }
 
-  static void handleManual() {
-    //esp_now_message.sendMessage();
-    mode = commun_MANUAL;
-    Serial.print("Manual");
-  }
+    static void handleManual()
+    {
+        // esp_now_message.sendMessage();
+        mode = commun_MANUAL;
+        Serial.print("Manual");
+    }
 
-  static void handleWall() {
-    //esp_now_message.sendMessage();
-    mode = commun_WALL;
-    Serial.print("Wall");
-  }
+    static void handleWall()
+    {
+        // esp_now_message.sendMessage();
+        mode = commun_WALL;
+        Serial.print("Wall");
+    }
 
-  static void handleCar() {
-    //esp_now_message.sendMessage();
-    mode = commun_PUSH;
-    Serial.print("Car");
-  }
+    static void handleCar()
+    {
+        // esp_now_message.sendMessage();
+        mode = commun_PUSH;
+        Serial.print("Car");
+    }
 
-  static void handleForward() {
-    //esp_now_message.sendMessage();
-    action = commun_FORWARD;
-    Serial.print("Forward");
-  }
+    static void handleForward()
+    {
+        // esp_now_message.sendMessage();
+        action = commun_FORWARD;
+        Serial.print("Forward");
+    }
 
-  static void handleSpeed() {
-    //esp_now_message.sendMessage();
-    speed = html_server.getVal();
-    Serial.printf("set speed %d",speed);
-  }
+    static void handleSpeed()
+    {
+        // esp_now_message.sendMessage();
+        speed = html_server.getVal();
+        Serial.printf("set speed %d", speed);
+    }
 
-  static void handleBackward() {
-    //esp_now_message.sendMessage();
-    action = commun_BACKWARD;
-    Serial.print("Backward");
-  }
+    static void handleBackward()
+    {
+        // esp_now_message.sendMessage();
+        action = commun_BACKWARD;
+        Serial.print("Backward");
+    }
 
-  static void handleLeft() {
-    //esp_now_message.sendMessage();
-    action = commun_LEFT;
-    Serial.print("Left");
-  }
+    static void handleLeft()
+    {
+        // esp_now_message.sendMessage();
+        action = commun_LEFT;
+        Serial.print("Left");
+    }
 
-  static void handleRight() {
-    //esp_now_message.sendMessage();
-    action = commun_RIGHT;
-    Serial.print("Right");
-  }
+    static void handleRight()
+    {
+        // esp_now_message.sendMessage();
+        action = commun_RIGHT;
+        Serial.print("Right");
+    }
 
-  static void handleStop() {
-    //esp_now_message.sendMessage();
-    action = commun_STOP;
-    Serial.print("Stop");
-  }
+    static void handleStop()
+    {
+        // esp_now_message.sendMessage();
+        action = commun_STOP;
+        Serial.print("Stop");
+    }
 
-  static void handleTurnRate() {
-    //esp_now_message.sendMessage();
-    turnRate = html_server.getVal();
-    Serial.printf("set TurnRate %d", turnRate);
-  }
+    static void handleTurnRate()
+    {
+        // esp_now_message.sendMessage();
+        turnRate = html_server.getVal();
+        Serial.printf("set TurnRate %d", turnRate);
+    }
 };
 
 WiFiServer web_commun::wifi_server(80);
@@ -216,67 +247,73 @@ commun_Actions web_commun::action = commun_STOP;
 int web_commun::speed = 0;
 int web_commun::turnRate = 50;
 
-class UDP_broadcast {  // use mamber function sendXY to broadcast
+class UDP_broadcast
+{ // use mamber function sendXY to broadcast
 public:
-  int signalPin1 = 8;  // GPIO pin receiving signal from Vive circuit
-  int signalPin2 = 18;
+    int signalPin1 = 8; // GPIO pin receiving signal from Vive circuit
+    int signalPin2 = 18;
 
-  WiFiUDP UDPServer;
-  const char *ssid = "TP-Link_FD24";
-  const char *password = "65512111";
-  IPAddress target;  // broadcast mode is 255
-  IPAddress myIP;    // change our IP
-  IPAddress gateway_IP = IPAddress(192, 168, 4, 1);
-  IPAddress subnet_IP = IPAddress(255, 255, 255, 0);
-  WiFiServer server;
-  char udpBuffer[20];
-  char packetBuffer[20];
-  int GroupNumber = 30;
-  const int UDP_PACKET_SIZE = 20;
+    WiFiUDP UDPServer;
+    const char *ssid = "TP-Link_FD24";
+    const char *password = "65512111";
+    IPAddress target; // broadcast mode is 255
+    IPAddress myIP;   // change our IP
+    IPAddress gateway_IP = IPAddress(192, 168, 4, 1);
+    IPAddress subnet_IP = IPAddress(255, 255, 255, 0);
+    WiFiServer server;
+    char udpBuffer[20];
+    char packetBuffer[20];
+    int GroupNumber = 30;
+    const int UDP_PACKET_SIZE = 20;
 
-  UDP_broadcast(IPAddress IP):server(80) {
-    target = IPAddress(192, 168, 1, 255);
-    myIP = IP;
-  }
-
-  ~UDP_broadcast() {}
-
-  void initialBroadcast(){
-    Serial.begin(115200);
-    WiFi.mode(WIFI_AP);  // wifi in ap mode, no router
-    WiFi.softAPConfig(myIP, gateway_IP, subnet_IP);
-    WiFi.softAP(ssid, password);
-
-    IPAddress softAP_IP = WiFi.softAPIP();
-    Serial.print("\n AP IP address: HTML//");
-    Serial.print(softAP_IP);
-    Serial.print("\n SSID: ");
-    Serial.print(ssid);
-    Serial.print("\n Password: ");
-    Serial.print(password);
-
-    server.begin();
-
-    UDPServer.begin(2808);  // 2808 arbitrary UDP port#
-  }
-
-  void sendXY(int x, int y) {  // use in main function to broadcast XY
-    std::sprintf(udpBuffer, "%02d,%04d,%04d", GroupNumber, x, y);
-    UDPServer.beginPacket(target, 2808);  // send to UDPport 2808
-    UDPServer.printf("%s", udpBuffer);
-    UDPServer.endPacket();
-    Serial.println(udpBuffer);
-  }
-
-  void handleUDPServer() {
-    int cb = UDPServer.parsePacket();
-    if (cb) {
-      UDPServer.read(packetBuffer, UDP_PACKET_SIZE - 1);
-      Serial.println(packetBuffer);
-      return;
+    UDP_broadcast(IPAddress IP) : server(80)
+    {
+        target = IPAddress(192, 168, 1, 255);
+        myIP = IP;
     }
-    Serial.println("no data received");
-  }
+
+    ~UDP_broadcast() {}
+
+    void initialBroadcast()
+    {
+        Serial.begin(115200);
+        WiFi.mode(WIFI_AP); // wifi in ap mode, no router
+        WiFi.softAPConfig(myIP, gateway_IP, subnet_IP);
+        WiFi.softAP(ssid, password);
+
+        IPAddress softAP_IP = WiFi.softAPIP();
+        Serial.print("\n AP IP address: HTML//");
+        Serial.print(softAP_IP);
+        Serial.print("\n SSID: ");
+        Serial.print(ssid);
+        Serial.print("\n Password: ");
+        Serial.print(password);
+
+        server.begin();
+
+        UDPServer.begin(2808); // 2808 arbitrary UDP port#
+    }
+
+    void sendXY(int x, int y)
+    { // use in main function to broadcast XY
+        std::sprintf(udpBuffer, "%02d,%04d,%04d", GroupNumber, x, y);
+        UDPServer.beginPacket(target, 2808); // send to UDPport 2808
+        UDPServer.printf("%s", udpBuffer);
+        UDPServer.endPacket();
+        Serial.println(udpBuffer);
+    }
+
+    void handleUDPServer()
+    {
+        int cb = UDPServer.parsePacket();
+        if (cb)
+        {
+            UDPServer.read(packetBuffer, UDP_PACKET_SIZE - 1);
+            Serial.println(packetBuffer);
+            return;
+        }
+        Serial.println("no data received");
+    }
 };
 
 class Serial_commun
